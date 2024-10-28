e682d5503af65322e14a1592efedf596ace13c1b
Jeann <jeann.rezende99@gmail.com>
2024-10-28 20:31:19 -0300

    V1

---
+++ src/main/java/br/com/tech4jan/enuns/Status.java
@@ -0,0 +1,39 @@
+package br.com.tech4jan.enuns;
+
+import javax.print.attribute.standard.Media;
+
+public enum Status {
+
+    ABERTO(0, "BAIXA"),
+    ANDAMENTO(1, "ANDAMENTO"),
+    ENCERRADO(2, "ENCERRADO");
+
+    private Integer cod;
+    private String descricao;
+
+    private Status(Integer cod, String descricao) {
+        this.cod = cod;
+        this.descricao = descricao;
+    }
+
+    public Integer getCod() {
+        return cod;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public static Status toEnum(Integer cod) {
+        if (cod == null) {
+            return null;
+        }
+
+        for (Status x : Status.values()) {
+            if (cod.equals(x.getCod())) {
+                return x;
+            }
+        }
+        throw new IllegalArgumentException("Status inválido!" + cod);
+    }
+}
