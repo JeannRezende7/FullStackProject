046c7cc94068afe79627d99bfd4504a1e4294a55
Jeann <jeann.rezende99@gmail.com>
2024-10-28 19:27:15 -0300

    NovoCommit

---
+++ src/main/java/br/com/tech4jan/fullstack/FullstackApplication.java
@@ -0,0 +1,13 @@
+package br.com.tech4jan.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tech4jan/fullstack/FullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tech4jan.fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
